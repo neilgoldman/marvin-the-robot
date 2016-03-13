10ae7b6926d680268f52e479b8ed3562c1f9ed93
Neil Goldman <neilgoldman@outlook.com>
2016-03-13 05:41:37 -0400

    First commit

---
+++ README.md
@@ -0,0 +1,10 @@
+Custom home automation and AI project.
+
+Currently supports speech recognition, with a hotword ("Marvin"). The eventual goal is to give it some personality (akin to Marvin from The Hitchiker's Guide To The Galaxy).
+
+Uses ouimeaux to control Belkin WeMo powerswitches.
+SpeechRecognition with CMUSphinx for speech recognition.
+
+No instructions on how to build and install dependencies, sorry. Alot of this was based in tis article, so it might help. 
+https://ggulati.wordpress.com/2016/02/24/coding-jarvis-in-python-3-in-2016/
+
---
+++ marvin.py
@@ -0,0 +1,189 @@
+import speech_recognition
+from speech_recognition import AudioSource, WaitTimeoutError
+import pyttsx
+import threading
+import itertools
+import requests
+import re
+from datetime import datetime
+
+"""
+A lot of the code and library choices are from
+https://ggulati.wordpress.com/2016/02/24/coding-jarvis-in-python-3-in-2016/
+
+"""
+
+wemo_api_url = "http://localhost:5000/api/device/"
+bedroom_lamp_str = 'bedroomlamp'
+speakers_str = 'speakers'
+
+speech_engine = pyttsx.init('espeak')
+speech_engine.setProperty('rate', 150)
+
+class Marvin:
+	_heard_name = False
+	_time_heard_name = None
+	match_words = []
+	
+	@staticmethod 
+	def compile_match_words():
+		print('Compiling keywords.')
+		lights_re = re.compile(r'.*\b(lights?|lamp)\b.*')
+		speakers_re = re.compile(r'.*\b(speakers?|monitors?)\b.*')
+		on_re = re.compile(r'.*\b(on)\b.*')
+		off_re = re.compile(r'.*\b(off)\b.*')
+		toggle_re = re.compile(r'.*\b(get|hit|switch|toggle)\b.*')
+		Marvin.match_words = (lights_re, speakers_re, on_re, off_re, toggle_re)
+		print('Done')
+		
+	@staticmethod
+	def heard_name():
+		Marvin._heard_name = True
+		Marvin._time_heard_name = datetime.now()
+		print('I heard my name!\n')
+	
+	@staticmethod
+	def check_if_still_listening():
+		if not Marvin._heard_name:
+			return False
+		if Marvin._time_heard_name and (datetime.now() - Marvin._time_heard_name).seconds > 20:
+			Marvin._heard_name = False
+		return Marvin._heard_name
+
+
+def find_microphone(sample_rate, chunk_size):
+	for i, mic_name in enumerate(speech_recognition.Microphone.list_microphone_names()):
+		if 'USB audio' in mic_name:
+			return speech_recognition.Microphone(device_index=i, sample_rate=sample_rate, chunk_size=chunk_size)
+	raise Exception("Could not find microphone")
+
+def speak(text):
+	speech_engine.say(text)
+	speech_engine.runAndWait()
+
+def listen(mic, recognizer=speech_recognition.Recognizer()):
+	stop_callback = custom_listen_in_background(recognizer, mic, recognize_async)
+	print('Listening.')
+	return stop_callback
+
+def recognize_async(recognizer, audio_data):
+	print('Recognizer callback')
+	decoder = recognize_sync(recognizer, audio_data, show_all=True)
+	possible_keywords = []
+
+	match_words = Marvin.match_words
+	heard_marvin = False
+
+	for best in itertools.islice(decoder.nbest(), 0, recognizer.num_n_best):
+		words = best.hypstr
+		if not words:
+			print('no words found')
+			break
+		# print(words)
+		if any(name in words for name in ('arvin', 'artin', 'marlin', 'marten', 'margaret')):
+			heard_marvin = True
+		for match_re in match_words:
+			m = match_re.match(words)
+			if m:
+				possible_keywords.append(m.group(1))
+	
+	if heard_marvin:
+		Marvin.heard_name()
+	elif not Marvin.check_if_still_listening():
+		return
+	
+	possible_keywords = ' '.join(possible_keywords)
+	on_count = possible_keywords.count(' on ')
+	off_count = possible_keywords.count(' off ')
+	toggle_count = sum(map(possible_keywords.count, (' get' , ' hit ', ' switch ', ' toggle ')))
+	
+	print(possible_keywords)
+	print(on_count)
+	print(off_count)
+	print(toggle_count)
+	
+	if (on_count + off_count + toggle_count) < 3:
+		return
+	
+	device_str = ''
+	if ('speaker' in possible_keywords) or ('monitor' in possible_keywords):
+		device_str = speakers_str
+	if ('light' in possible_keywords) or ('lamp' in possible_keywords):
+		device_str = bedroom_lamp_str
+	
+	if device_str:
+		if on_count > off_count:
+			print('Turning ' + device_str + ' on')
+			requests.post(wemo_api_url + device_str, data = {'state': 'on'})
+		elif off_count > on_count:
+			print('Turning ' + device_str + ' off')
+			requests.post(wemo_api_url + device_str, data = {'state': 'off'})
+		elif toggle_count > (on_count + off_count):
+			print('Toggling ' + device_str)
+			requests.post(wemo_api_url + device_str, data = {'state': 'toggle'})
+
+
+def recognize_sync(recognizer, audio_data, show_all=False):
+		try:
+			return recognizer.recognize_sphinx(audio_data, show_all=show_all)
+		except speech_recognition.UnknownValueError:
+			print("Could not understand audio")
+			return None
+		except speech_recognition.RequestError as e:
+			print("Recog Error; {0}".format(e))
+			return None
+		return None
+
+
+def custom_listen_in_background(recognizer, source, callback):
+	assert isinstance(source, AudioSource), "Source must be an audio source"
+	running = [True]
+	def threaded_listen():
+		with source as s:
+			while running[0]:
+				try: # listen for 1 second, then check again if the stop function has been called
+					audio = recognizer.listen(s, 3)
+				except WaitTimeoutError: # listening timed out, just try again
+					print('Adjusting for ambient noise')
+					recognizer.adjust_for_ambient_noise(s, duration=0.5)
+					recognizer.energy_threshold = recognizer.energy_threshold - (recognizer.energy_threshold * 0.35)
+					print(recognizer.energy_threshold)
+				else:
+					if running[0]: callback(recognizer, audio)
+	def stopper():
+		running[0] = False
+		listener_thread.join() # block until the background thread is done, which can be up to 1 second
+	listener_thread = threading.Thread(target=threaded_listen)
+	listener_thread.daemon = True
+	listener_thread.start()
+	return stopper
+	
+
+def main():
+	Marvin.compile_match_words()
+	print('Opening Microphone')
+	mic = find_microphone(sample_rate=32000, chunk_size=1024)
+	print('Loading Recognizer')
+	recognizer = speech_recognition.Recognizer()
+	recognizer.energy_threshold = 100
+	recognizer.dynamic_energy_threshold = True
+	recognizer.dynamic_energy_adjustment_damping = 0.2
+	recognizer.dynamic_energy_adjustment_ratio = 1.2
+	recognizer.pause_threshold = 0.3
+	recognizer.non_speaking_duration = 0.2
+	recognizer.num_n_best = 40
+
+	print('Streaming from Microphone.')
+	stop_listening= listen(mic, recognizer=recognizer)
+	from time import sleep
+	try:
+		while True:
+			sleep(1)
+	except KeyboardInterrupt:
+		print('Handling quit command')
+		stop_listening()
+		exit()
+
+
+if __name__ == '__main__':
+	main()
